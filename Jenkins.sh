2b7845ca46fd7e809eafe5d266e715f5b12ecf0f
mayank64857 <34202239+mayank64857@users.noreply.github.com>
2021-04-28 14:47:10 +0530

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -1,4 +1,12 @@
-echo "Hello NAB!"
-echo "how are you ?"
-
-echo "Free space is" {df -kh |grep /dev/xvda1 |cut -d " " -f10}
+#!/bin/sh
+df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
+do
+  echo $output
+  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
+  partition=$(echo $output | awk '{ print $2 }' )
+  echo "Disk Space is fine total $usep percent used in /root on $(hostname) as on $(date)"
+  if [ $usep -ge 20 ]; then
+    echo "Running out of space \"$partition ($usep%)\ on $(hostname) as on $(date)" |
+     mail -s "Alert: Almost out of disk space $usep%" xyz@abc.com
+  fi
+done
